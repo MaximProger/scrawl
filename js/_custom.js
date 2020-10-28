840f91d4610a6945bb08b74631f5c660cb6962fe
Maksim Koshman <maksimkoshman090@gmail.com>
2020-10-28 13:14:12 +0300

    desktop version is ready

---
+++ js/_custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+
+  /* сохраняем текстовое поле в переменную text */
+  var text = document.getElementById("inputText");
+
+  /* сохраняем кнопку в переменную btn */
+  var btn = document.getElementById("copyText");
+
+  /* вызываем функцию при нажатии на кнопку */
+  btn.onclick = function () {
+    text.select();
+    document.execCommand("copy");
+  };
+});
